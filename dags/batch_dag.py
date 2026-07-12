1bd0b728d179e9b6bd5408d72223015425e19a5c
Mohammed Luqman <152679427+MohammedLuqman@users.noreply.github.com>
2026-07-12 16:24:14 +0300

    Update batch_dag.py

--- dags/batch_dag.py
+++ dags/batch_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime, timedelta
 
 default_args = {
@@ -13,18 +14,22 @@ default_args = {
 with DAG(
     'silver_layer_pipeline',
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule_interval=None,  
     catchup=False
 ) as dag:
 
     submit_job = SparkSubmitOperator(
-        application="/opt/airflow/scripts/batch_job.py",  
         task_id="run_spark_silver_process",
-        conn_id="spark_default",  
+        application="/opt/airflow/scripts/batch_job.py",  
+        conn_id="spark_default", 
+        jars="/opt/airflow/jars/*", 
         conf={
             "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
             "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
             "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
-        },
-        jars="/home/jovyan/jars/*"
+            "spark.hadoop.fs.s3a.access.key": "admin",
+            "spark.hadoop.fs.s3a.secret.key": "minio_password",
+            "spark.hadoop.fs.s3a.path.style.access": "true",
+            "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem"
+        }
     )
